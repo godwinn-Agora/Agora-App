2cae068adab48c917ff72a29634ab2c370bfd7c6
Godwill <agoraproject.godwinn@gmail.com>
2025-07-26 12:16:22 +0100

    résolution init_db.py

--- init_db.py
+++ init_db.py
@@ -1,7 +1,7 @@
-# init_db.py
 from app import app, db
+from models import Utilisateur  # même si grisé, à garder
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        print("Tables créées.")
+        print("✅ Tables créées avec succès !")
